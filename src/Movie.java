abbeb2818fa9d2f0f96323829f12690871e11ba4
d-wagner <daniel.wagner@net-z.one>
2017-05-04 14:34:58 +0200

    refactoring inheritance

--- src/Movie.java
+++ src/Movie.java
@@ -4,18 +4,23 @@ public class Movie {
     public static final int REGULAR = 0;
     public static final int NEW_RELEASE = 1;
     private String title;
-    private int priceCode;
+    private Price price;
 
     public Movie(String newtitle, int newpriceCode) {
         title = newtitle;
-        priceCode = newpriceCode;
+        setPriceCode(newpriceCode);
     }
     public int getPriceCode() {
-        return priceCode;
+        return price.getPriceCode();
     }
 
     public void setPriceCode(int arg) {
-        priceCode = arg;
+        switch (arg){
+            case Movie.REGULAR: price = new RegularPrice(); break;
+            case Movie.CHILDRENS: price = new ChildrensPrice(); break;
+            case Movie.NEW_RELEASE: price = new NewPrice(); break;
+            default: throw new IllegalArgumentException("Incorrect Price Code");
+        }
     }
 
     public String getTitle (){
@@ -23,29 +28,10 @@ public class Movie {
     };
 
     public double getCharge(int daysRented){
-        double result = 0;
-        switch (this.priceCode) {
-            case Movie.REGULAR:
-                result += 2;
-                if (daysRented > 2)
-                    result += (daysRented - 2) * 1.5;
-                break;
-            case Movie.NEW_RELEASE:
-                result += daysRented * 3;
-                break;
-            case Movie.CHILDRENS:
-                result += 1.5;
-                if (daysRented > 3)
-                    result += (daysRented - 3) * 1.5;
-                break;
-        }
-        return result;
+        return price.getCharge(daysRented);
     }
 
     public int getFrequentRenterPoints(int daysRented){
-        if ((this.priceCode == Movie.NEW_RELEASE) && daysRented > 1)
-            return 2;
-        else
-            return 1;
+        return price.getFrequentRenterPoints(daysRented);
     }
 }
---
+++ src/Price.java
@@ -0,0 +1,68 @@
+/**
+ * Created by D065058 on 04.05.2017.
+ */
+abstract class Price {
+
+    abstract int getPriceCode();
+
+    abstract double getCharge(int daysRented);
+
+    public int getFrequentRenterPoints(int daysRented){
+        return 1;
+    }
+
+}
+
+class ChildrensPrice extends Price{
+
+    @Override
+    int getPriceCode() {
+        return Movie.CHILDRENS;
+    }
+
+    @Override
+    public double getCharge(int daysRented) {
+        double result = 1.5;
+        if (daysRented > 3)
+            result += (daysRented - 3) * 1.5;
+        return result;
+    }
+}
+
+class RegularPrice extends Price{
+
+    @Override
+    int getPriceCode() {
+        return Movie.REGULAR;
+    }
+
+    @Override
+    public double getCharge(int daysRented){
+        double result = 2;
+        if (daysRented > 2)
+            result += (daysRented - 2) * 1.5;
+        return result;
+    }
+}
+class NewPrice extends Price{
+
+    @Override
+    int getPriceCode() {
+        return Movie.NEW_RELEASE;
+    }
+
+    @Override
+    public double getCharge(int daysRented) {
+        return daysRented * 3;
+    }
+
+    @Override
+    public int getFrequentRenterPoints(int daysRented) {
+        if (daysRented > 1){
+            return 2;
+        }
+        else {
+            return 1;
+        }
+    }
+}
